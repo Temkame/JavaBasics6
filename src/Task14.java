88fd41c22fa435992a4cec2db8dc534a5855aaa7
temkame <temkame@yandex.ru>
2022-03-27 20:53:48 +0300

    Update

---
+++ src/Task14.java
@@ -0,0 +1,10 @@
+/*
+Вывести на экран сегодняшнюю дату
+
+Вывести на экран текущую дату в аналогичном виде «21 06 2015».
+ */
+public class Task14 {
+    public static void main(String[] args) {
+        System.out.println("21 06 2015");
+    }
+}
